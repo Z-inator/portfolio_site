998f2bf893605031ad0d4d7d636a6ddcb1a5e427
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-09-07 13:21:17 -0500

    Redesigning Layout around Screen Size breaking points

--- lib/main.dart
+++ lib/main.dart
@@ -11,33 +11,86 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.cyan,
-      ),
-      home: PCScreen()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+        ),
+        home: ResponsiveScreen());
   }
 }
 
-class PCScreen extends StatelessWidget {
-  const PCScreen({Key? key}) : super(key: key);
+class ResponsiveScreen extends StatelessWidget {
+  const ResponsiveScreen({Key? key}) : super(key: key);
+
+  Widget getScreen(Size screenSize) {
+    if (screenSize.width >= 1200) {
+      return LargeScreen();
+    } else if (screenSize.width > 800 && screenSize.width < 1200) {
+      return MediumScreen();
+    } else {
+      return SmallScreen();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return getScreen(MediaQuery.of(context).size);
+  }
+}
+
+class SmallScreen extends StatelessWidget {
+  const SmallScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: SafeArea(
-        child: Scaffold(
-          body: Row(
-            children: [
-              LeftNavBar(),
-              Expanded(child: MainContentBody())
-            ],
-          ),
-        )),
+          child: Scaffold(
+              appBar: AppBar(),
+              body: Column(
+                children: [Expanded(child: MainContentBody())],
+              ))),
     );
   }
 }
 
+class MediumScreen extends StatelessWidget {
+  MediumScreen({Key? key}) : super(key: key);
+  int selectedIndex = 0;
 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SafeArea(
+          child: Scaffold(
+        body: Row(
+          children: [
+            LeftNavBar(),
+            VerticalDivider(),
+            Expanded(child: MainContentBody())
+          ],
+        ),
+      )),
+    );
+  }
+}
 
+class LargeScreen extends StatelessWidget {
+  const LargeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SafeArea(
+          child: Scaffold(
+        body: Row(
+          children: [
+            LeftNavBar(),
+            // VerticalDivider(),
+            Expanded(child: MainContentBody())
+          ],
+        ),
+      )),
+    );
+  }
+}
